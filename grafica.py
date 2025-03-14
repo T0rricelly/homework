34435bca3b8e2c195bbc03855f20ea22559be316
robinpuentes <robinpuentess@gmail.com>
2025-03-14 15:47:37 -0500

    prueba 3

---
+++ grafica.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+x = [0, 5,7,0]
+y = [0, 3,1,0]
+
+plt.plot(x, y , marker="o", color="r")
+plt.xlabel("Lado")
+plt.ylabel("Lado2")
+plt.title("Triangulo")
+plt.show()
+
+# pruba de git
+dv segtbv4r
+
+
