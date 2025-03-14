4737765f3179cf48a86166db46052cf1d41c5216
santiago <kevintorres1609@gmail.com>
2025-03-14 14:58:48 -0500

    graficas

--- grafica.py
+++ grafica.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-
 x = [0, 5,7,0]
 y = [0, 3,1,0]
 
-plt.plot(x, y , marker="o", color="g")
+plt.plot(x, y , marker="o", color="r")
+plt.xlabel("Lado")
+plt.ylabel("Lado2")
+plt.title("Triangulo")
 plt.show()
